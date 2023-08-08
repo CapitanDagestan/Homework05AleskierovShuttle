107e154f579bbff66599d031c7d87001aae7c4f3
CapitanDagestan <aleskerovbogdan@gmail.com>
2023-08-08 22:03:25 +0300

    Redo all

--- src/Shuttles.java
+++ src/Shuttles.java
@@ -1,16 +1,21 @@
-import java.util.Random;
-
 public class Shuttles {
     public static void main(String[] args) {
-        Random random = new Random();
-        int[] lucky = {1, 2, 3, 5, 6, 7, 8};
-        int[] luckyNull = {0, 1, 2, 3, 5, 6, 7, 8};
+        int shuttleNum = 1;
         for (int i = 0; i < 100; i++) {
-            int r = random.nextInt(7);
-            int r1 = random.nextInt(8);
-            int r2 = random.nextInt(8);
-            int[] shuttleNum = {lucky[r], luckyNull[r1], luckyNull[r2]};
-            System.out.println(String.valueOf(shuttleNum[0]) + shuttleNum[1] + shuttleNum[2]);
+            System.out.println(shuttleNum);
+            shuttleNum++;
+            if ((shuttleNum - 4) % 10 == 0) {
+                shuttleNum++;
+            }
+            if ((shuttleNum - 9) % 10 == 0) {
+                shuttleNum++;
+            }
+            if ((shuttleNum - 100 * (shuttleNum / 100)) / 10 == 4) {
+                shuttleNum += 10;
+            }
+            if ((shuttleNum - 100 * (shuttleNum / 100)) / 10 == 9) {
+                shuttleNum += 10;
+            }
         }
     }
 }
